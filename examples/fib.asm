f9d38fbdd5a4a6a47c738bbf66a850499c523329
akifsah <04makifsahin@proton.me>
2026-01-26 15:17:56 +0300

    fix example fib.asm

--- examples/fib.asm
+++ examples/fib.asm
@@ -1,22 +1,18 @@
-addi s4 zero 0 
-addi s5 zero 1 
+    li a0 20
 
-addi s2 zero 0 
-store 0 s2 s4
+    mv t0 a0
+    addi t0 t0 -2
+    li t1 1
+    li t2 2
 
-addi s2 zero 1 
-store 0 s2 s5
+    addi t0 t0 -1
+    add t1 t1 t2
 
-addi s2 zero 2 
-addi s3 zero 11
+    xor t1 t1 t2
+    xor t2 t1 t2
+    xor t1 t1 t2
+    bne t0 zero -5
 
-add s6 s4 s5
-store 0 s2 s6
-add s4 zero s5
-add s5 zero s6
-addi s2 s2 1 
-
-blt s2 s3 -6
-
-end
-
+    mv a0 t2
+    sw t2 10(zero)
+    end
