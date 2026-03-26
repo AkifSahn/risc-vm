1d48e48bebb4fdaa92402766259d98559688cbb0
akifsah <04makifsahin@proton.me>
2026-03-26 18:17:08 +0300

    small fix

--- vm/interface.go
+++ vm/interface.go
@@ -1,12 +1,12 @@
 package vm
 
 type Vm_State struct {
-	Pc        uint32            `json:"pc"`
-	Cycle     int               `json:"cycle"`
-	Registers map[uint8]int32   `json:"registers"`
-	Memory    map[uint32][]byte `json:"memory"`
-	CycleInfo Cycle_Info        `json:"cycle_info"`
-	Halt      bool              `json:"halt"`
+	Pc        uint32          `json:"pc"`
+	Cycle     int             `json:"cycle"`
+	Registers map[uint8]int32 `json:"registers"`
+	Memory    map[uint32]byte `json:"memory"`
+	CycleInfo Cycle_Info      `json:"cycle_info"`
+	Halt      bool            `json:"halt"`
 }
 
 func (v *Vm) GetState() Vm_State {
@@ -15,14 +15,14 @@ func (v *Vm) GetState() Vm_State {
 		Pc:        v.Pc,
 		Cycle:     int(v.Dm.N_cycle),
 		Registers: map[uint8]int32{},
-		Memory:    map[uint32][]byte{},
+		Memory:    map[uint32]byte{},
 		CycleInfo: v.Dm.Cycle_infos[len(v.Dm.Cycle_infos)-1],
 		Halt:      v._halt,
 	}
 
 	// TODO: Fix, load byte by byte not word
 	for _, addr := range v.Memory_diff_addr {
-		state.Memory[addr] = v.Memory[addr: addr+4]
+		state.Memory[addr] = v.Memory[addr]
 	}
 
 	for _, reg := range v.Register_diff_idx {
