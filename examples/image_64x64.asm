8b6f05b5cd6724d4203488b95f3c79cfc02bc703
akifsah <04makifsahin@proton.me>
2026-03-27 17:06:11 +0300

    modified example

--- examples/image_64x64.asm
+++ examples/image_64x64.asm
@@ -1,16 +1,16 @@
-; Requires minimum of 64*64*4*4 = 65,536 Bytes of memory
+; Requires minimum of 64*64*4 = 16,384 Bytes of memory
 
-li a0, 64       ; x
-li a1, 64       ; y
-li t0, 255 ; temp reg to store maximum pixel value
+li a0, 64  ; x
+li a1, 64  ; y
+li t0, 255 ; Alpha value
 loop_y:
     li a0, 64
     loop_x:
-        addi sp, sp, -16    ; reserve 4 words
-        sw a0, 0(sp)        ; R
-        sw zero, 4(sp)        ; G
-        sw a1, 8(sp)        ; B
-        sw t0, 12(sp)       ; A
+        addi sp, sp, -4    ; reserve 1 word
+        sb a0, 0(sp)       ; R
+        sb zero, 1(sp)     ; G
+        sb a1, 2(sp)       ; B
+        sb t0, 3(sp)       ; A
         addi a0, a0, -1
         bne a0, zero, loop_x   ; keep looping while a0 != 0
     addi a1, a1, -1
